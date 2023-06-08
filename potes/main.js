54f898692e9c78a23870079e768fddb51fc1111d
nope <nope>
2023-06-08 23:44:26 +0100

    added torneio

--- potes/main.js
+++ potes/main.js
@@ -1,6 +1,8 @@
 const addPotsButton = document.getElementById('addPots');
 const potsInputField = document.getElementById('potsInputField');
 
+const predefButton = document.getElementById('predefButton');
+
 const potsArray = [];
 
 function createPots(pots) {
@@ -40,49 +42,53 @@ function createPots(pots) {
         potContainer.appendChild(playerContainer);
 
         button.addEventListener('click', () => {
-
-            const playerDiv = document.createElement('div');
-            playerDiv.classList.add('player-name-container');
-
-            const player = document.createElement('span');
-            player.classList.add('player-name');
-            const playerName = textInput.value.substring(0, 15);
-            player.innerText = playerName;
-
-            potsArray[index].push(playerName);
-
-            const cross = document.createElement('a');
-            cross.classList.add('close');
-            cross.href = '#';
-
-            const hr = document.createElement('hr');
-            cross.addEventListener('click', () => {
-                playerContainer.removeChild(playerDiv);
-                playerContainer.removeChild(hr);
-                const playerIndex = potsArray[index].indexOf(playerName);
-                potsArray[index].splice(playerIndex, 1);
-            })
-
-            playerDiv.appendChild(player);
-            playerDiv.appendChild(cross);
-            playerContainer.appendChild(playerDiv);
-            playerContainer.appendChild(hr);
-            textInput.value = '';
+            addPlayerToPot(index, playerContainer, textInput);
         });
 
         textInput.addEventListener('keypress', (event) => {
             if (event.key == 'Enter') {
                 button.click();
             }
-        })
+        });
 
     }
+
     const createTeamsButton = document.createElement('button');
     createTeamsButton.innerText = 'Create Teams';
     createTeamsButton.classList.add('addButton');
     createTeamsButton.id = 'createTeamsButton';
     createTeamsButton.addEventListener('click', createTeams);
     document.getElementById('createTeamsButton').appendChild(createTeamsButton);
+}
+
+function addPlayerToPot(index, playerContainer, textInput) {
+    const playerDiv = document.createElement('div');
+    playerDiv.classList.add('player-name-container');
+
+    const player = document.createElement('span');
+    player.classList.add('player-name');
+    const playerName = textInput.value.substring(0, 15);
+    player.innerText = playerName;
+
+    potsArray[index].push(playerName);
+
+    const cross = document.createElement('a');
+    cross.classList.add('close');
+    cross.href = '#';
+
+    const hr = document.createElement('hr');
+    cross.addEventListener('click', () => {
+        playerContainer.removeChild(playerDiv);
+        playerContainer.removeChild(hr);
+        const playerIndex = potsArray[index].indexOf(playerName);
+        potsArray[index].splice(playerIndex, 1);
+    })
+
+    playerDiv.appendChild(player);
+    playerDiv.appendChild(cross);
+    playerContainer.appendChild(playerDiv);
+    playerContainer.appendChild(hr);
+    textInput.value = '';
 }
 
 addPotsButton.addEventListener('click', () => {
@@ -105,7 +111,7 @@ function addPlayer() {
 
 }
 
-function createTeams() {
+async function createTeams() {
     const teams = [[], []];
     let assingTo = 0;
     for (let index = 0; index < potsArray.length; index++) {
@@ -136,6 +142,7 @@ function createTeams() {
             playerDiv.classList.add('teamPlayer');
             playerDiv.innerText = player;
             teamDiv.appendChild(playerDiv)
+            await wait(1000);
         }
 
     }
@@ -151,4 +158,57 @@ function shuffle(a) {
         a[j] = x;
     }
     return a;
+}
+
+predefButton.addEventListener('click', async () => {
+    document.getElementById('potsSelector').style.display = 'none';
+
+    const players = [
+        [
+            'Manu',
+            'Gabigol',
+            'Luis Santos',
+            'Kiko',
+            'Luis Costa',
+            'Cenz',
+        ],
+        [
+            'Hugo',
+            'Fred',
+            'Alheira',
+            'Berto',
+            'Joao Ferreira',
+            'Miguel Costa',
+        ],
+        [
+            'Ibra',
+            'KS',
+            'Rada',
+            'Gandi',
+            'Miguel Coelho',
+        ]
+    ]
+
+
+    createPots(3);
+    const inputs = document.querySelectorAll('.inputField');
+    for (let index = 1; index <= 3; index++) {
+        console.log(players);
+        for (const player of players[index-1]) {
+            console.log(player);
+            inputs[index].value = player;
+            inputs[index].nextElementSibling.click();
+            // await wait(100);
+        }
+    }
+
+});
+
+
+function wait(ms) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
 }
